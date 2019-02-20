0c6243ab2c193c69d8747d1c90185736952011b8
Sara Bahrini <sara@smal37-bianca.local>
2019-02-20 16:23:19 +0100

    initial commit

---
+++ iTrip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iTrip
+//
+//  Created by Sara Bahrini on 2/20/19.
+//  Copyright © 2019 Sarimon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
