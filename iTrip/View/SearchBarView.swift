71a8891eb388d6ae2d007647e179468f32a4663d
Simon Rothert <puresamari@gmail.com>
2019-04-13 13:42:19 +0200

    Neuer shit

--- iTrip/View/SearchBarView.swift
+++ iTrip/View/SearchBarView.swift
@@ -8,58 +8,16 @@
 
 import UIKit
 
-class CustomIntensityVisualEffectView: UIVisualEffectView {
-    init(effect: UIVisualEffect, intensity: CGFloat) {
-        super.init(effect: nil)
-        animator = UIViewPropertyAnimator(duration: 1, curve: .linear) { [unowned self] in self.effect = effect }
-        animator.fractionComplete = intensity
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError()
-    }
-    
-    private var animator: UIViewPropertyAnimator!
-}
-
 @IBDesignable
-class SearchBarView: UIView {
+class SearchBarView: BlurryBackgroundView {
 
     override func draw(_ rect: CGRect) {
-        backgroundColor = .clear
-        let blurEffect = UIBlurEffect(style: .extraLight)
-        let blurView = CustomIntensityVisualEffectView(effect: blurEffect, intensity: 0.3)
-        blurView.frame = self.frame
-        blurView.translatesAutoresizingMaskIntoConstraints = false
-        
-        self.insertSubview(blurView, at: 0)
-        
-        NSLayoutConstraint.activate([
-            blurView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
-            blurView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
-            blurView.topAnchor.constraint(equalTo: self.topAnchor),
-            blurView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
-        ])
-        
-        let vibrancyEffect = UIVibrancyEffect(blurEffect: blurEffect)
-        let vibrancyView = UIVisualEffectView(effect: vibrancyEffect)
-        vibrancyView.translatesAutoresizingMaskIntoConstraints = false
-        
-        blurView.contentView.addSubview(vibrancyView)
-        
-        NSLayoutConstraint.activate([
-            vibrancyView.heightAnchor.constraint(equalTo: blurView.contentView.heightAnchor),
-            vibrancyView.widthAnchor.constraint(equalTo: blurView.contentView.widthAnchor),
-            vibrancyView.centerXAnchor.constraint(equalTo: blurView.contentView.centerXAnchor),
-            vibrancyView.centerYAnchor.constraint(equalTo: blurView.contentView.centerYAnchor)
-        ])
+        super.draw(rect)
         
         layer.cornerRadius = frame.height / 2
         
         layer.borderWidth = 1
         
         layer.borderColor = UIColor.black.cgColor
-    
-        clipsToBounds = true
     }
 }
